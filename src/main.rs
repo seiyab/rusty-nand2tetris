e6883b0e870c2699bf2d1fb9b72073355733b3be
seiya <r108338@yahoo.co.jp>
2021-12-27 22:40:02 +0900

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 mod primitive;
 
 fn main() {
-  let a = primitive::Pin::Negative;
-  let b = primitive::Pin::Positive;
-  let _ = a.nand(b);
-  println!("Hello, world!");
+    let a = primitive::Pin::Negative;
+    let b = primitive::Pin::Positive;
+    let _ = a.nand(b);
+    println!("Hello, world!");
 }
--- src/primitive/mod.rs
+++ src/primitive/mod.rs
@@ -1,29 +1,29 @@
 #[derive(Clone, Copy)]
 pub enum Pin {
-  Positive,
-  Negative,
+    Positive,
+    Negative,
 }
 
 impl Pin {
-  pub fn nand(&self, another: Pin) -> Pin {
-    match self {
-      Self::Positive => match another {
-        Self::Positive => Self::Negative,
-        Self::Negative => Self::Positive,
-      },
-      Self::Negative => Self::Positive,
+    pub fn nand(&self, another: Pin) -> Pin {
+        match self {
+            Self::Positive => match another {
+                Self::Positive => Self::Negative,
+                Self::Negative => Self::Positive,
+            },
+            Self::Negative => Self::Positive,
+        }
     }
-  }
 }
 
 mod tests {
-  use super::Pin;
+    use super::Pin;
 
-  #[test]
-  fn nand_works() {
-    assert!(matches!(Pin::Positive.nand(Pin::Positive), Pin::Negative));
-    assert!(matches!(Pin::Positive.nand(Pin::Negative), Pin::Positive));
-    assert!(matches!(Pin::Negative.nand(Pin::Positive), Pin::Positive));
-    assert!(matches!(Pin::Negative.nand(Pin::Negative), Pin::Positive));
-  }
+    #[test]
+    fn nand_works() {
+        assert!(matches!(Pin::Positive.nand(Pin::Positive), Pin::Negative));
+        assert!(matches!(Pin::Positive.nand(Pin::Negative), Pin::Positive));
+        assert!(matches!(Pin::Negative.nand(Pin::Positive), Pin::Positive));
+        assert!(matches!(Pin::Negative.nand(Pin::Negative), Pin::Positive));
+    }
 }
