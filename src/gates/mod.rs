9610f432ecba59812fd27676d1d3ce56a355af2d
seiya <r108338@yahoo.co.jp>
2021-12-27 22:54:27 +0900

    implement or

--- src/gates/mod.rs
+++ src/gates/mod.rs
@@ -8,6 +8,10 @@ pub fn and(x: Pin, y: Pin) -> Pin {
     not(x.nand(y))
 }
 
+pub fn or(x: Pin, y: Pin) -> Pin {
+    not(x).nand(not(y))
+}
+
 mod test {
     use super::super::primitive::Pin;
     use super::*;
@@ -24,5 +28,13 @@ mod test {
         assert!(matches!(and(Pin::Positive, Pin::Negative), Pin::Negative));
         assert!(matches!(and(Pin::Negative, Pin::Positive), Pin::Negative));
         assert!(matches!(and(Pin::Negative, Pin::Negative), Pin::Negative));
+    }
+
+    #[test]
+    fn or_works() {
+        assert!(matches!(or(Pin::Positive, Pin::Positive), Pin::Positive));
+        assert!(matches!(or(Pin::Positive, Pin::Negative), Pin::Positive));
+        assert!(matches!(or(Pin::Negative, Pin::Positive), Pin::Positive));
+        assert!(matches!(or(Pin::Negative, Pin::Negative), Pin::Negative));
     }
 }
